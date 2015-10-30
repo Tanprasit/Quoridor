5f66098ef0283fbce58acfef736a5b86fb89e64c
Luke Tanprasit <tanprasitp@hotmail.co.uk>
2015-10-30 16:28:45 +0000

    First commit

---
+++ src/players/QuoridorPlayer.java
@@ -0,0 +1,34 @@
+package players;
+
+import quoridor.GameDisplay;
+import quoridor.GameState2P;
+import quoridor.Quoridor;
+
+/**
+ *
+ * @author steven
+ */
+public abstract class QuoridorPlayer {
+    
+    protected GameState2P state;
+    protected GameDisplay display;    
+    protected Quoridor game;
+    int index;
+        
+    public QuoridorPlayer(GameState2P state, int index, Quoridor game){
+        this.state=state;                             
+        this.index = index;
+        this.game=game;
+    }     
+ 
+    public void setState(GameState2P state){
+        this.state=state;
+    }
+    
+   public void setDisplay(GameDisplay display) {
+        this.display = display;        
+    }
+    
+    public abstract void chooseMove();
+    
+}
---
+++ src/quoridor/Quoridor.java
@@ -0,0 +1,55 @@
+package quoridor;
+
+import players.QuoridorPlayer;
+import players.HumanPlayer;
+import players.BasicComputerPlayer;
+
+/**
+ *
+ * @author steven
+ */
+public class Quoridor {
+
+    GameState2P state;
+    QuoridorPlayer[] players;
+    GameDisplay display;
+
+    public Quoridor() {
+        state = new GameState2P();
+        display = new GameDisplay(state);
+        players = new QuoridorPlayer[2];
+        players[0] = new HumanPlayer(state, 0, this);
+        players[1] = new BasicComputerPlayer(state, 1, this);
+        for (int i = 0; i < 2; i++) {
+            players[i].setDisplay(display);
+        }
+        players[0].chooseMove();
+    }
+
+    public void doMove(int playerIndex, GameState2P newState) {
+        state = newState;
+        for (int i = 0; i < 2; i++) {
+            players[i].setState(newState);
+        }
+        final int nextIndex = (playerIndex + 1) % 2;
+        display.updateState(newState);
+        if (!newState.isGameOver()) {
+            /*
+             * Run the method chooseMove in a separate thread
+             * to avoid the GUI from becoming unresponsive while 
+             * the next move by the computer player is being computed
+             */ 
+            new Thread() {
+                public void run() {
+                    players[nextIndex].chooseMove();
+                }
+            }.start();
+        }
+        else
+            java.awt.Toolkit.getDefaultToolkit().beep();
+    }
+
+    public static void main(String[] args) {
+        new Quoridor();
+    }
+}
